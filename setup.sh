1f16e5c4a15d5ff7543dcec389eb0a17ef43099f
pycon <pycon@debian>
2015-11-08 11:04:10 +0800

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+adduser pycon sudo # need to logout after this
+
+# run the following with sudo
+
+apt-get install python-pip virtualenv
+apt-get install build-essential python-dev
+mkdir ~/breaking
+cd ~/breaking
+
+pip install beautifulsoup4
+pip install selenium
+
+# pip install numpy scipy matplotlib
+
+# Due to Laziness, use apt-get to install numpy scipy matplotlib and opencv
+
+sudo apt-get install python-numpy python-scipy python-matplotlib python-pandas python-sympy python-nose python-opencv ipython-notebook
+
